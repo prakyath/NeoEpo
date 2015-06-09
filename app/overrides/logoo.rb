d541320acbd908a35c737ba6cbd0fca10514693e
prakyath <prakyathshankesi@gmail.com>
2015-06-09 19:34:29 +0530

    deleted first ovverride

--- app/overrides/logoo.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_header",
-                     :replace => "[id='logo']",
-                     :name => "logo",
-                     :partial =>"logo_content")
