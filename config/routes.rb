b5b9b361f884fd3129ebaf6f31d1552a65d326d2
prakyath <prakyathshankesi@gmail.com>
2015-06-10 13:55:44 +0530

    coming soon page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
 
+  get 'comingsoon/index'
+
   # This line mounts Spree's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-  mount Spree::Core::Engine, :at => '/'
+ 
+  root to:"comingsoon#index"
+
+ #  mount Spree::Core::Engine, :at => '/'
+       
+
+
+
+
+
+
+
+
           # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
